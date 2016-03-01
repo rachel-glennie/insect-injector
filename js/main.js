7f12a766a487e930937496d52ac7aae0a0c697fe
Rachel Glennie <glen0038@algonquinlive.com>
2016-03-01 17:24:57 -0500

    Finalized other insects

--- js/main.js
+++ js/main.js
@@ -5,13 +5,46 @@ var insects = [
     img: 'ant-nest-beetle.jpg',
     family: 'Carabidae',
     size: [6, 20],
-    food:{
+    food: {
       'eatsInsects': true,
       'eatsDung': false,
       'eatsPlants': false,
     }
-  }
-]
+  },
+  {
+    name: 'Dung Beetle',
+    img: 'dung-beetle.jpg',
+    family: 'Scarabidae',
+    size: [20, 30],
+    food: {
+      'eatsInsects': false,
+      'eatsDung': true,
+      'eatsPlants': false,
+    }
+  },
+  {
+    name: 'Stag Beetle',
+    img: 'stag-beetle.jpg',
+    family: 'Lucanidae',
+    size: [5, 120],
+    food: {
+      'eatsInsects': false,
+      'eatsDung': false,
+      'eatsPlants': true,
+    }
+  },
+  {
+    name: 'Tortoise Beetle',
+    img: 'tortoise-beetle.jpg',
+    family: 'Cassidinae',
+    size: [1, 18],
+    food: {
+      'eatsInsects': false,
+      'eatsDung': false,
+      'eatsPlants': true,
+    }
+  },
+];
 
 // ##########################################################
 // START DO NOT CHANGE ZONE
